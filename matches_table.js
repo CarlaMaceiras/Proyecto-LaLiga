43d0fbe32f605ce2adbcca65151a47f21aaa0879
Carla Maceiras <carla.maceiras@gmail.com>
2021-05-19 17:12:58 +0200

    filtros

--- matches_table.js
+++ matches_table.js
@@ -1,8 +1,17 @@
 tablaEquipos(data.matches);
 
+let boton = document.getElementById("botonFiltro");
+
+boton.addEventListener("click", () => filtrarNombres(data.matches));
+
+// let checkbox= document.getElementById("box");
+
+// checkbox.addEventListener("click", () => filtroCheckbox (data.matches));
+
 function tablaEquipos(partidos) {
 
     let tabla = document.querySelector("#body_table");
+    tabla.innerHTML = "";
 
     for (let i = 0; i < partidos.length; i++) {
 
@@ -11,23 +20,23 @@ function tablaEquipos(partidos) {
         let equipoLocal = document.createElement("td");
         equipoLocal.innerHTML = partidos[i].homeTeam.name;
 
-        let escudoLocal= document.createElement ("td");
-        
-        let imagenLocal=document.createElement ("img")
-        imagenLocal.setAttribute ("src", "https://crests.football-data.org/" + partidos[i].homeTeam.id + ".svg" );
+        let escudoLocal = document.createElement("td");
+
+        let imagenLocal = document.createElement("img")
+        imagenLocal.setAttribute("src", "https://crests.football-data.org/" + partidos[i].homeTeam.id + ".svg");
 
         let resultado = document.createElement("td");
 
         if (partidos[i].score.fullTime.homeTeam == null) {
-            resultado.innerHTML= " Próximamente "
-        } else{
+            resultado.innerHTML = " Próximamente "
+        } else {
             resultado.innerHTML = partidos[i].score.fullTime.homeTeam + " - " + partidos[i].score.fullTime.awayTeam;
         }
 
-        let escudoVisitante= document.createElement ("td");
-        
-        let imagenVisitante=document.createElement ("img");
-        imagenVisitante.setAttribute ("src", "https://crests.football-data.org/" + partidos[i].awayTeam.id + ".svg" );
+        let escudoVisitante = document.createElement("td");
+
+        let imagenVisitante = document.createElement("img");
+        imagenVisitante.setAttribute("src", "https://crests.football-data.org/" + partidos[i].awayTeam.id + ".svg");
 
         let equipovisitante = document.createElement("td");
         equipovisitante.innerHTML = partidos[i].awayTeam.name;
@@ -39,5 +48,56 @@ function tablaEquipos(partidos) {
 
     }
 
+
+
+
+
 }
+
+function filtrarNombres(equipos) {
+
+    let formulario = document.querySelector("#formulario");                                   //para que cuando se escriba y demos al botón, detecte el texto 
+    let texto = formulario.value.toLowerCase();                                              //se guarda lo que el usuario escribe en el input en minúsculas
+    let nuevaLista = equipos.filter(equipo => {
+
+        if (equipo.homeTeam.name.toLowerCase().includes(texto) || equipo.awayTeam.name.toLowerCase().includes(texto)) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+    tablaEquipos(nuevaLista);
+
+}
+
+// function filtroCheckbox(opciones) {
+
+//     let todos = document.querySelector("#box_all");
+
+//     let ganados = document.querySelector("#box_win");
+
+//     let perdidos = document.querySelector("#box_lost");
+
+//     let empatados = document.querySelector("#box_draw");
+
+//     let próximos = document.querySelector("#box_next");
+
+//     todos.addEventListener("click", opciones.filter(opcion => {
+
+
+//     })
+
+    
+
+// }
+
+
+
+
+
+
+
+
+
 
